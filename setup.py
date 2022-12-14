d1f19cf1cbd9bd0142ab94f53eaf58d1a1fdb18d
Luciana Trento <luciana.traineri@gmail.com>
2022-12-14 18:29:53 -0500

    chore: Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def README():
 
 setup(
     name="psindex",
-    version="0.0.1",
+    version="0.0.2",
     author="Luciana Trento Raineri",
     author_email="luciana.traineri@gmail.com",
     url="https://github.com/luraineri/psindex",
