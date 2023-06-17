0c4dd1d66b08993a9c724e166e7ee8bdc16c8568
Bruno Amaral <amaralbf@gmail.com>
2023-06-17 17:38:07 -0300

    test: Add identical property test for continuous

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -3,7 +3,7 @@ from hypothesis import strategies as st
 from hypothesis.extra.pandas import range_indexes, series
 from numpy.testing import assert_almost_equal
 
-from psindex.psi import psi_cat
+from psindex.psi import psi_cat, psi_cont
 
 
 @given(
@@ -28,3 +28,13 @@ def test_commutative_property(a, b):
 )
 def test_identical_samples_property_for_categorical_psi(a):
     assert_almost_equal(psi_cat(a, a), 0)
+
+
+@given(
+    series(
+        elements=st.integers(min_value=0, max_value=3),
+        index=range_indexes(min_size=20, max_size=50),
+    ),
+)
+def test_identical_samples_property_for_continuous_psi(a):
+    assert_almost_equal(psi_cont(a, a, nbins=5), 0)
