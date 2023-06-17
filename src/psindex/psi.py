26ef1b6d3a89b365deeb14cb4c58bedbced2fe76
Luciana Trento <luciana.traineri@gmail.com>
2023-06-17 15:19:19 -0400

    Feat: First continuous versions

--- src/psindex/psi.py
+++ src/psindex/psi.py
@@ -17,3 +17,29 @@ def psi_cat(sample_a, sample_b):
     psi = sum((ai - bi) * np.log(ai / bi))
 
     return psi
+
+
+def psi_cont(sample_a, sample_b, bins):
+    nai = np.histogram(sample_a, bins)[0]
+    nbi = np.histogram(sample_b, bins)[0]
+
+    ai = nai / nai.sum() 
+    bi = nbi / nbi.sum()
+
+    psi = sum((ai - bi) * np.log(ai / bi))
+
+    return psi
+
+def psi_cont2(sample_a, sample_b, bucket_type, nbins):
+    d = {'sample_a': sample_a, 'sample_b': sample_b}
+    df = pd.DataFrame(data=d)
+    
+    if bucket_type == 'bins':
+        ser, bins = pd.cut(df['sample_a'], bins=nbins, retbins=True)
+    elif bucket_type == 'quantiles':
+        ser, bins = pd.qcut(df["sample_a"], q=nbins, retbins=True)
+    
+    df['bucket_a'] = pd.cut(df.sample_a, bins=bins)
+    df['bucket_b'] = pd.cut(df.sample_b, bins=bins)
+
+    return psi_cat(df['bucket_a'] , df['bucket_b'])
