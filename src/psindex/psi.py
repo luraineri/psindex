11308be16bfc8d0df906561a033e396a17a0f74d
Luciana Trento <luciana.traineri@gmail.com>
2023-06-24 14:42:56 -0400

    feat: Update psi_cat, add dropna parameter and add tests for missing values

--- src/psindex/psi.py
+++ src/psindex/psi.py
@@ -2,29 +2,44 @@ import numpy as np
 import pandas as pd
 
 
-def psi_cat(sample_a, sample_b):
-    # calc nai, nbi, Na, Nb - considerando categorias de 'a'
-    categories_a = sample_a.unique()
+MISSING_CATEGORY_FREQ = 0.001
 
-    nai = sample_a.value_counts()
-    nbi = sample_b.value_counts()
 
-    ai = [nai[cat] / len(sample_a) for cat in categories_a]
-    bi = [nbi[cat] / len(sample_b) for cat in categories_a]
-    ai = np.array(ai)
-    bi = np.array(bi)
+def calc_freq(sample_a, sample_b, dropna):
+    freq_a = sample_a.value_counts(normalize=True, dropna=dropna)
+    freq_b = sample_b.value_counts(normalize=True, dropna=dropna)
 
-    # calc PSI
-    psi = sum((ai - bi) * np.log(ai / bi))
+    # categories with zero count appear in value_counts result if the series
+    # come from pd.cut. Here we remove them
+    freq_a = freq_a[freq_a > 0]
+    freq_b = freq_b[freq_b > 0]
+
+    categories_a = freq_a.index
+    categories_b = freq_b.index
+
+    missing_categories = set(categories_a) - set(categories_b)
+
+    for cat in missing_categories:
+        freq_b[cat] = MISSING_CATEGORY_FREQ
+
+    freq_b = freq_b[categories_a.to_list()]
+
+    return freq_a, freq_b
+
+
+def psi_cat(sample_a, sample_b, dropna=True):
+    freq_a, freq_b = calc_freq(sample_a, sample_b, dropna)
+
+    psi = sum((freq_a - freq_b) * np.log(freq_a / freq_b))
 
     return psi
 
 
-def psi_cont(sample_a, sample_b, nbins: int):
+def psi_cont(sample_a, sample_b, nbins: int, dropna=True):
 
     _, bins_edges = pd.cut(sample_a, bins=nbins, retbins=True)
 
     bucket_a = pd.cut(sample_a, bins=bins_edges)
     bucket_b = pd.cut(sample_b, bins=bins_edges)
 
-    return psi_cat(bucket_a, bucket_b)
+    return psi_cat(bucket_a, bucket_b, dropna)
--- tests/test_psi.py
+++ tests/test_psi.py
@@ -1,8 +1,9 @@
+import numpy as np
 import pandas as pd
 import pytest
 from numpy.testing import assert_almost_equal
 
-from psindex.psi import psi_cat
+from psindex.psi import psi_cat, psi_cont
 
 from .utils import make_series
 
@@ -32,3 +33,79 @@ def test_psi_cat_samples(a, b, exp_result):
 def test_same_distribution_series():
     s = make_series({"a": 10, "b": 20, "c": 30})
     assert_almost_equal(psi_cat(s, pd.concat([s] * 10)), 0)
+
+
+@pytest.mark.parametrize(
+    ["a", "b", "exp_result"],
+    [
+        (
+            make_series({"a": 3, "b": 2, "c": 3, "d": 1}),
+            make_series({"a": 2, "b": 2, "c": 3, np.nan: 2}),
+            0.565913,
+        ),
+        (
+            make_series({"a": 2, "b": 2, "c": 3, np.nan: 2}),
+            make_series({"a": 3, "b": 2, "c": 3, "d": 1}),
+            0.047231,
+        ),
+        (
+            make_series({"a": 3, "b": 2, "c": 3, "d": 1, np.nan: 2}),
+            make_series({"a": 3, "b": 2, "c": 3, "d": 1, np.nan: 2}),
+            0,
+        ),
+    ],
+)
+def test_missing_values_cat(a, b, exp_result):
+    assert_almost_equal(psi_cat(a, b), exp_result, decimal=6)
+
+
+def test_same_proportion_cont():
+    a = make_series({1: 3, 5: 3, 10: 3})
+    b = make_series({1: 1, 5: 1, 10: 1})
+
+    assert_almost_equal(psi_cont(a, b, nbins=3), 0, decimal=6)
+
+
+@pytest.mark.parametrize(
+    ["a", "b", "exp_result", "dropna"],
+    [
+        (
+            make_series({1: 3, 5: 3, 10: 3, np.nan: 10}),
+            make_series({1: 1, 5: 1, 10: 1}),
+            0,
+            True,
+        ),
+        (
+            make_series({1: 1, 5: 1, 10: 1}),
+            make_series({1: 3, 5: 3, 10: 3, np.nan: 10}),
+            0,
+            True,
+        ),
+        (
+            make_series({1: 1, 5: 1, 10: 1, np.nan: 1000}),
+            make_series({1: 3, 5: 3, 10: 3, np.nan: 10}),
+            0,
+            True,
+        ),
+        (
+            make_series({1: 3, 5: 3, 10: 3, np.nan: 10}),
+            make_series({1: 1, 5: 1, 10: 1}),
+            3.684847,
+            False,
+        ),
+        (
+            make_series({1: 1, 5: 1, 10: 1}),
+            make_series({1: 3, 5: 3, 10: 3, np.nan: 10}),
+            0.393270,
+            False,
+        ),
+        (
+            make_series({1: 1, 5: 1, 10: 1, np.nan: 1000}),
+            make_series({1: 3, 5: 3, 10: 3, np.nan: 10}),
+            2.684731,
+            False,
+        ),
+    ],
+)
+def test_missing_values_cont(a, b, exp_result, dropna):
+    assert_almost_equal(psi_cont(a, b, nbins=3, dropna=dropna), exp_result, decimal=6)
